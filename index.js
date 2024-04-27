107449308de5d80d212151ba63ec827ddd8c3c70
React-projects <avishwakarma8855@gmail.com>
2024-04-27 18:03:51 +0530

    form check

--- index.js
+++ index.js
@@ -28,6 +28,29 @@ app.post('/register',function(req,res){
         email: req.body.email
     }
 })
+
+app.post ('/test' , (req , res , next)=>{
+
+    console.log(req.body);
+const qno_arr = [2, 3, 5, 7, 9];
+const checkboxwt = [10, 8, 5, 3];
+let sum = 0;
+
+// for (let i = 0; i < 5; i++) {
+//     for (let j = 1; j < 5; j++) {
+//         if (req.body[`input${i + 1}_${j}`] ) {
+//             sum += checkboxwt[j - 1] * qno_arr[i];
+//             break;
+//         }
+//     }
+// }
+
+
+
+
+})
+
+
 app.get('/register',function(req,res){
     res.render("Register")
 });
