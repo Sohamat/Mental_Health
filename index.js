4c51b0e4d8394cc9d92eb133e90ed0c1db525cc5
Sohamat <sohamvichare8@gmail.com>
2024-04-27 18:04:24 +0530

    5th

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express= require('express');
 const app= express();
 const path= require('path');
  
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')))
 
-const fs= require('fs');
 app.set('view engine','ejs');
+
 
 app.get('/',function(req,res){
     res.render("index")
